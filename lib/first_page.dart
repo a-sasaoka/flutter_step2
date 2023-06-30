1f7aa0a943dd4f28859dfb5709cc9f0888563a95
a-sasaoka <chikezo@gmail.com>
2023-06-30 18:56:24 +0900

    Enhance

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -16,48 +16,64 @@ class FirstPage extends StatelessWidget {
       ),
       body: Center(
         child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              /*
-              const Image(
-                image: NetworkImage(
-                    'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+            padding: const EdgeInsets.all(16.0),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+/*                
+                  const Image(
+                    image: NetworkImage(
+                        'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+                  ),
+ */
+                  Image.network(
+                      'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+                  const Divider(
+                    color: Colors.black,
+                  ),
+                  TextField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: '何か入力してください',
+                    ),
+                    onChanged: (text) {
+                      nameText = text;
+                    },
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                //fullscreenDialog: true,
+                                builder: (context) => SecondPage(nameText),
+                              ));
+                        },
+                        child: const Text('次の画面へ'),
+                      ),
+                      const SizedBox(
+                        width: 16,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                fullscreenDialog: true,
+                                builder: (context) => ThirdPage(),
+                              ));
+                        },
+                        child: const Text('リスト表示'),
+                      ),
+                    ],
+                  ),
+                ],
               ),
-              */
-              Image.network(
-                  'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
-              TextField(
-                onChanged: (text) {
-                  nameText = text;
-                },
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        //fullscreenDialog: true,
-                        builder: (context) => SecondPage(nameText),
-                      ));
-                },
-                child: const Text('次の画面へ'),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        fullscreenDialog: true,
-                        builder: (context) => ThirdPage(),
-                      ));
-                },
-                child: const Text('リスト表示'),
-              ),
-            ],
-          ),
-        ),
+            )),
       ),
     );
   }
